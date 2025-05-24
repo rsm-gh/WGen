3cae652cafbfbd4a118e183b70b112e72f3c3e14
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2025-05-24 19:54:28 +0200

    New Install/Uninstall scripts

---
+++ install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+#
+# This file is part of WGen.
+#
+#  Copyright (C) 2025 Rafael Senties Martinelli.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+#
+# Remove previous versions
+#
+echo -e "Removing previous versions..."
+chmod a+x "$DIR/uninstall.sh"
+"$DIR/uninstall.sh"
+
+#
+# Install the new files
+#
+echo -e "\nInstalling the software files..."
+
+function install_files(){
+
+    echo "installing: $1"
+
+    if [ -d "$DIR$1/__pycache__" ]; then
+      rm -rf "$DIR$1/__pycache__"
+    fi
+
+    install -d "$1" |& grep -v "omitting directory"
+    install -D "$DIR$1/"* "$1" |& grep -v "omitting directory"
+
+}
+
+install_files "/usr/bin"
+install_files "/usr/share/doc/wgen"
+install_files "/usr/share/man/man8"
+install_files "/usr/share/wgen"
+install_files "/usr/share/wgen/test"
---
+++ uninstall.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# This file is part of Wgen.
+#
+#  Copyright (C) 2025 Rafael Senties Martinelli.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root."
+  exit
+fi
+
+function remove_file(){
+
+    if [ -f "$1" ]; then
+        rm -f "$1"
+        echo "removed.f: $1"
+    fi
+}
+
+function remove_dir(){
+
+    if [ -d "$1" ]; then
+        rm -rf "$1"
+        echo "removed.d: $1"
+    fi
+}
+
+remove_dir "/usr/share/wgen"
+remove_dir "/usr/share/doc/wgen"
+remove_file "/usr/share/man/man8/wgen.8"
+remove_file "/usr/bin/wgen"
